beaddc2f6d48aad0d4403c4fbcfc67e44f431140
Kristian Dahl Kærgaard <hcand.dk@gmail.com>
2018-08-25 10:34:33 +0200

    Generate default admin password from /dev/urandom

--- config/start.sh
+++ config/start.sh
@@ -4,7 +4,7 @@ sed -i \
     -e "s/#GAME_STYLE#/${GAME_STYLE:-"3"}/g" \
     -e "s/#MAX_PLAYERS#/${MAX_PLAYERS:-"32"}/g" \
     -e "s/#GAME_PASSWORD#/${GAME_PASSWORD:-""}/g" \
-    -e "s/#ADMIN_PASSWORD#/${ADMIN_PASSWORD:-"changeme123"}/g" \
+    -e "s/#ADMIN_PASSWORD#/${ADMIN_PASSWORD:-"$(< /dev/urandom tr -dc A-Za-z0-9 | head -c8; echo)"}/g" \
     -e "s/#SERVER_NAME#/${SERVER_NAME:-"Just another FFA"}/g" \
     -e "s/#BALANCE_TEAMS#/${BALANCE_TEAMS:-"1"}/g" soldat.ini
 
