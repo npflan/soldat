6b1a0fa8b8019ce5dfc5fe66adab654fe58baa5c
Kristian Dahl Kærgaard <hcand.dk@gmail.com>
2018-09-29 11:28:23 +0200

    Change default GameStyle and random password length

--- config/start.sh
+++ config/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 sed -i \
-    -e "s/#GAME_STYLE#/${GAME_STYLE:-"3"}/g" \
+    -e "s/#GAME_STYLE#/${GAME_STYLE:-"0"}/g" \
     -e "s/#MAX_PLAYERS#/${MAX_PLAYERS:-"32"}/g" \
     -e "s/#GAME_PASSWORD#/${GAME_PASSWORD:-""}/g" \
-    -e "s/#ADMIN_PASSWORD#/${ADMIN_PASSWORD:-"$(< /dev/urandom tr -dc A-Za-z0-9 | head -c8; echo)"}/g" \
+    -e "s/#ADMIN_PASSWORD#/${ADMIN_PASSWORD:-"$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32; echo)"}/g" \
     -e "s/#SERVER_NAME#/${SERVER_NAME:-"Just another FFA"}/g" \
     -e "s/#BALANCE_TEAMS#/${BALANCE_TEAMS:-"1"}/g" soldat.ini
 
